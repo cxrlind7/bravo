c8d43c9977b9b67c7da90a40b3848cb26b9f0230
DigitalesBack Desarrollo <digitalesback-desarrollo@devsecops.hdi.com.mx>
2025-10-11 09:34:30 -0600

    rutas

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,8 @@ const routes = [
     },
   ]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('#'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
